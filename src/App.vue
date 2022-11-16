d4bdf8008af33d9a94ee5734044aa882947deaad
Vladislav Maryutin <louietrueman@gmail.com>
2022-11-16 10:38:34 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <HeaderComponent />
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent
+  }
+}
+</script>
+
+<style>
+@import url('./fonts/fonts.css');
+
+#app {
+  display: flex;
+  flex-direction: column;
+  margin: 0 38px;
+  padding: 0;
+}
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,46 @@
+<template>
+	<header class="header">
+		<h1 class="header__title">
+			{{title}}
+		</h1>
+		<div>
+			<button class="header__button">
+				<img src="../images/sign_in_button.png" alt="">
+			</button>
+			<button class="header__button">
+				{{signUp.text}}
+			</button>
+			<button class="header__button">
+				{{info.text}}
+			</button>
+		</div>
+	</header>
+</template>
+
+<script>
+
+
+  export default {
+	name: 'HeaderComponent',
+	data() {
+		return {
+			title: 'Lectorium',
+			signIn: {
+				text: 'sign in'
+			},
+			signUp: {
+				text: 'sign up'
+			},
+			info: {
+				text: 'info'
+			}
+		}
+	}
+  }
+</script>
+
+<style scoped>
+  @import url('../styles/header/header.css');
+  @import url('../styles/header/header__title.css');
+  @import url('../styles/header/header__button.css');
+</style>
